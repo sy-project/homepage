ea7f7b1690ec4bfcc96933a90905bc8f1c932327
Ai93012 <105848529+Ai93012@users.noreply.github.com>
2024-10-31 23:40:01 +0900

    Add files via upload

---
+++ test_file/script.js
@@ -0,0 +1,18 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 7000);
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1;
+    showSlides();
+}
